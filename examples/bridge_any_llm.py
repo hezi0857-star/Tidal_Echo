39fb01e3fdcd717c0acaf18bd5aafcb5a116768d
hezi0857-star <hezi0857@gmail.com>
2026-06-28 22:31:52 +0800

    Update bridge_any_llm.py

--- examples/bridge_any_llm.py
+++ examples/bridge_any_llm.py
@@ -77,6 +77,7 @@ if not PERSONA and _persona_file:
         pass
 if not PERSONA:
     PERSONA = "你是对方的 AI 伴侣,在一个私密的一对一聊天里。说话自然、简短、有温度,像在用手机聊天,不要长篇大论。"
+    print(">>> PERSONA:", repr(PERSONA[:100]))
 
 # 模型链:主模型 + 可选兜底(LLM_*_2 / _3)。任一返回 FALLBACK_CODES 就顺次切下一个。
 def _model_routes() -> list:
